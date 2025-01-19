74f60235ad982e849cd6bbe51c772e411c810a29
Claudio Benedetti <claudio.benedetti@mia-platform.eu>
2025-01-19 14:26:01 +0100

    feat: week / month views WIP: responsive header

--- src/components/HabitCalendar.vue
+++ src/components/HabitCalendar.vue
@@ -8,9 +8,12 @@ import ButtonGroup from 'primevue/buttongroup';
 
 import { computed, ref } from 'vue';
 
+type View = 'week' | 'month'
+
 type Props = {
   entries: Entry[]
   onAddEntry: (date: Dayjs) => void
+  initialView?: View
 }
 
 type Slot = {
@@ -20,9 +23,22 @@ type Slot = {
 
 const props = defineProps<Props>()
 
-const currentRange = ref<[Dayjs, Dayjs]>([now().startOf('month'), now().endOf('month')])
+const view = ref<View>(props.initialView ?? 'month')
 
-const currentMonthName = computed(() => currentRange.value[0].format('MMMM YYYY'))
+const currentRange = ref<[Dayjs, Dayjs]>([
+  now().startOf(view.value),
+  now().endOf(view.value)
+])
+
+const currentMonthName = computed(() => {
+  return currentRange.value[0].format('MMM YYYY')
+})
+
+const currRangeName = computed(() => {
+  return currentRange.value[0].format('ddd D')
+    + ' - '
+    + currentRange.value[1].format('ddd D')
+})
 
 const entriesByIsoDate = computed(() =>
   props.entries.reduce<Record<string, Entry[]>>((acc, entry) => {
@@ -46,56 +62,76 @@ const slots = computed<Slot[]>(() => {
 })
 
 const slotsWithPadding = computed<(Slot | null)[]>(() => {
+  if (view.value === 'week') {
+    return slots.value
+  }
   const firstDay = currentRange.value[0].day()
   const firstDayOffset = (firstDay === 0 ? 6 : firstDay - 1)
   return [...Array(firstDayOffset).fill(null), ...slots.value]
 })
 
-const prevMonth = () => {
+const toPrev = () => {
   const [oldStart] = currentRange.value
-  const newStart = oldStart.add(-1, 'day').startOf('month')
-  const newEnd = newStart.endOf('month')
+  const newStart = oldStart.add(-1, 'day').startOf(view.value)
+  const newEnd = newStart.endOf(view.value)
   currentRange.value = [newStart, newEnd]
 }
-const nextMonth = () => {
+
+const toNext = () => {
   const [, oldEnd] = currentRange.value
-  const newStart = oldEnd.add(1, 'day').startOf('month')
-  const newEnd = newStart.endOf('month')
+  const newStart = oldEnd.add(1, 'day').startOf(view.value)
+  const newEnd = newStart.endOf(view.value)
   currentRange.value = [newStart, newEnd]
 }
-const currMonth = () => {
-  currentRange.value = [now().startOf('month'), now().endOf('month')]
+const toToday = () => {
+  currentRange.value = [now().startOf(view.value), now().endOf(view.value)]
+}
+const changeView = () => {
+  view.value = view.value === 'month' ? 'week' : 'month'
+  currentRange.value = [now().startOf(view.value), now().endOf(view.value)]
 }
 </script>
+
+<!-- TODO: week view should start from monday -->
 
 <template>
   <div class="calendar-container">
     <div class="header">
       <ButtonGroup>
         <Button
-          @click="prevMonth"
+          @click="toPrev"
           icon="pi pi-caret-left"
           severity="secondary"
           variant="outlined"
         />
         <Button
-          @click="nextMonth"
+          @click="toNext"
           icon="pi pi-caret-right"
           severity="secondary"
           variant="outlined"
         />
+        <Button
+          @click="changeView"
+          :label="view === 'month' ? 'week' : 'month'"
+          severity="secondary"
+          variant="outlined"
+        />
       </ButtonGroup>
-      <h2>{{ currentMonthName }}</h2>
+      <h2>{{ currentMonthName }} {{currRangeName}}</h2>
       <Button
-        @click="currMonth"
+        @click="toToday"
         label="Today"
         severity="secondary"
         variant="outlined"
         rounded
-        :disabled="now().month() === currentRange[0].month()"
+        :disabled="
+          (view === 'month' && now().month() === currentRange[0].month())
+          || (view === 'month' && now().startOf('week') === currentRange[0])
+        "
       />
     </div>
-    <div class="calendar">
+    
+    <div v-if="view === 'month'" class="month-calendar">
       <div v-for="index in countTo(6)" :key="index" class="ellipsable">
         <span>{{ weekDays[index] }}</span>
       </div>
@@ -122,6 +158,29 @@ const currMonth = () => {
         </template>
       </div>
     </div>
+    <div v-else-if="view === 'week'" class="week-calendar">
+      <template v-for="(slot, index) in slotsWithPadding" :key="index">
+        <span v-if="slot" class="weekday">{{ slot.date.format('ddd D') }}</span>
+        <div
+          v-if="slot"
+          :class="'day'+`${slot?.date && isSameDay(now(), slot.date) ? ' today' : ''}`"
+        >
+            <div class="day-body">
+              <div v-for="{id} in slot.entries" :key="id" class="event-indicator" />
+            </div>
+            <div class="day-footer">
+              <Button
+                @click="onAddEntry(slot.date)"
+                icon="pi pi-plus"
+                size="small"
+                variant="text"
+              />
+            </div>
+        </div>
+      </template>
+    </div>
+
+
   </div>
 </template>
 
@@ -145,47 +204,59 @@ const currMonth = () => {
     }
   }
 
-  .calendar {
+  .week-calendar {
+    display: grid;
+    grid-template-columns: 1fr 5fr;
+    grid-template-rows: repeat(2, 1fr);
+    gap: var(--p-gap-s);
+    align-items: center;
+    width: inherit;
+    
+    .weekday {
+      white-space: nowrap;
+    }
+  }
+
+  .month-calendar {
     display: grid;
     grid-template-columns: repeat(7, 1fr);
     gap: var(--p-gap-s);
     width: inherit;
-    
-    .day {
+  }
+
+  .day {
+    display: flex;
+    flex-direction: column;
+
+    border: 1px solid #ddd;
+    text-align: center;
+    border-radius: 5px;
+    min-height: 5rem;
+
+    &.today {
+      /* TODO handle bright mode */
+      background-color: var(--p-surface-700);
+    }
+
+    .day-body {
       display: flex;
       flex-direction: column;
+      gap: var(--p-gap-xs);
+      flex-grow: 1;
+      padding: var(--p-padding-xs);
 
-      position: relative;
-      border: 1px solid #ddd;
-      text-align: center;
-      border-radius: 5px;
-      min-height: 5rem;
-
-      &.today {
-        /* TODO handle bright mode */
-        background-color: var(--p-surface-700);
+      .event-indicator {
+        width: 100%;
+        height: 10px;
+        background-color: var(--p-primary-500);
+        border-radius: 3px;
       }
+    }
 
-      .day-body {
-        display: flex;
-        flex-direction: column;
-        gap: var(--p-gap-xs);
-        flex-grow: 1;
-        padding: var(--p-padding-xs);
-
-        .event-indicator {
-          width: 100%;
-          height: 10px;
-          background-color: var(--p-primary-500);
-          border-radius: 3px;
-        }
-      }
-
-      .day-footer {
-        button {
-          width: 100%;
-          height: 1rem;
-        }
+    .day-footer {
+      button {
+        width: 100%;
+        height: 1rem;
       }
     }
   }
